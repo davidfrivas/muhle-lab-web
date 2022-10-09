21c2296eca8595fea5ed4ca1112d897eae654318
davidfrivas <dfr5108@gmail.com>
2022-10-09 03:29:54 -0400

    Add JS functionality to sidebar navbar dropdown

--- nav.js
+++ nav.js
@@ -12,3 +12,16 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-expanded", false);
     }
 });
+
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdown = document.querySelector(".dropdown-content");
+
+dropdownButton.addEventListener("click", () => {
+    const dropdownVisibility = dropdown.getAttribute("menu-visible")
+    
+    if (dropdownVisibility === "false") {
+        dropdown.setAttribute("menu-visible", true);
+    } else if (dropdownVisibility === "true") {
+        dropdown.setAttribute("menu-visible", false);
+    }
+})
